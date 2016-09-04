78327813d53aa4c02e6d73d128c7332d2e7f720a
Armin Braun <me@obrown.io>
2016-09-04 22:09:07 +0200

    Fix xb build

--- xb/run.sh
+++ xb/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -e
 ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
 cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
 echo -e 'Host localhost *\n\tStrictHostKeyChecking no\n' >> ~/.ssh/config
