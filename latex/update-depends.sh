21691a2b1d99c18349229ef988d70151ab3a6cbb
Yegor Bugayenko <yegor256@gmail.com>
2025-03-02 13:10:05 +0300

    update fix

--- latex/update-depends.sh
+++ latex/update-depends.sh
@@ -3,15 +3,17 @@
 # SPDX-FileCopyrightText: Copyright (c) 2012-2025 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
-if [ ! -e DEPENDS.txt ]; then
-    echo 'There is no DEPENDS.txt file in the current directory'
+depends=$1
+
+if [ ! -e "${depends}" ]; then
+    echo "The file is absent: ${depends}"
     exit 1
 fi
 
 tlmgr option repository ctan
 tlmgr --verify-repo=none update --self
 
-readarray -t packages < <(cut -d' ' -f2 DEPENDS.txt | uniq)
+readarray -t packages < <(cut -d' ' -f2 "${depends}" | uniq)
 
 tlmgr --verify-repo=none install "${packages[@]}"
 tlmgr --verify-repo=none update "${packages[@]}"
