c9486b1258c381c2bb8f210242582584cab783f7
Yegor Bugayenko <yegor256@gmail.com>
2025-03-13 13:57:18 +0300

    uninstall

--- ruby/install-chrome.sh
+++ ruby/install-chrome.sh
@@ -12,6 +12,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 apt-get update --yes --fix-missing
 apt-get install --yes google-chrome-stable
+apt-get remove --yes --purge chromium-driver
 
 google-chrome --version
 /usr/bin/google-chrome --version
