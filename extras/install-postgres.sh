a0d28ad38cb1c002779bd06cd1d1cfbcb8160c0a
Yegor Bugayenko <yegor256@gmail.com>
2026-04-16 18:38:11 +0300

    Update PostgreSQL installation script to version 16.

--- extras/install-postgres.sh
+++ extras/install-postgres.sh
@@ -5,17 +5,19 @@
 
 set -ex -o pipefail
 
+ver=16
+
 wget -qO - https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/postgresql-archive-keyring.gpg
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+
 apt-get update --yes --fix-missing
 apt-get install --yes libpq-dev
-apt-get install --yes postgresql-client-16 postgresql-16
-dir=$(realpath "/usr/lib/postgresql"/*)
-version=$(basename "${dir}")
-apt-get install --yes "postgresql-server-dev-${version}"
+apt-get install --yes "postgresql-client-${ver}" "postgresql-${ver}"
+dir="/usr/lib/postgresql/${ver}"
+apt-get install --yes "postgresql-server-dev-${ver}"
 apt-get clean
 
-for c in initdb postgres pg_ctl; do
+for c in initdb postgres pg_ctl createdb; do
     ln -s "$(realpath "${dir}/bin/${c}")" "/bin/${c}"
 done
 
