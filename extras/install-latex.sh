79113b4a25395aa2bf7d582d573d6809549e8a23
Yegor Bugayenko <yegor256@gmail.com>
2026-05-08 19:30:24 +0300

    Update installation script to add Tex Live to system PATH.

--- extras/install-latex.sh
+++ extras/install-latex.sh
@@ -22,13 +22,15 @@ ln -s "$(ls "/usr/local/texlive/${year}/bin/")" "/usr/local/texlive/${year}/bin/
 cd /tmp
 rm -rf ./install-tl*
 
-echo "export PATH=\${PATH}:/usr/local/texlive/${year}/bin/latest" >> "${HOME}/.profile"
+echo "export PATH=\${PATH}:/usr/local/texlive/${year}/bin/latest" > /etc/profile.d/texlive.sh
+chmod +x /etc/profile.d/texlive.sh
 export PATH="${PATH}:/usr/local/texlive/${year}/bin/latest"
 
 tlmgr init-usertree
 tlmgr option repository "${url}"
 tlmgr --verify-repo=none install \
   texliveonfly collection-latex luaotfload l3kernel l3packages latexmk l3build biber xetex collection-fontsrecommended
+tlmgr path add
 chmod -R a+w /usr/local/texlive
 
 pdflatex --version
